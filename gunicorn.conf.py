215f33fe6f967814c952b6b6e6b0e6bc00e449c6
Sendhan <sendhan12345@gmail.com>
2026-05-16 21:41:24 +0530

    fix: use gevent worker instead of eventlet

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,9 @@
 import os
 bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
 workers = 1
-worker_class = "eventlet"
+worker_class = "gevent"
 worker_connections = 1000
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 graceful_timeout = 30
 accesslog = '-'
 errorlog = '-'
-worker_tmp_dir = '/dev/shm'
