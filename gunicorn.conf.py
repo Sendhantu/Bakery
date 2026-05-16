e414917df081be665b7e7555febecb7f256f2a67
Sendhan <sendhan12345@gmail.com>
2026-05-16 21:26:40 +0530

    fix: switch to eventlet worker, remove mysql and gevent deps

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,7 @@
 import os
-
-
 bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
-workers = int(os.environ.get('WEB_CONCURRENCY', '2'))
-threads = int(os.environ.get('GUNICORN_THREADS', '2'))
-worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
+workers = 1
+worker_class = "eventlet"
 worker_connections = 1000
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 graceful_timeout = 30
